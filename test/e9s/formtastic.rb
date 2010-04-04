9e9623cda2eb3b5ff88d349eb211cf2974ab41ad
Paul Engel <paulengel@ArCHanGeL.local>
2010-04-04 18:19:04 +0200

    Moved unit test files

---
+++ test/e9s/formtastic.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "test_helper.rb"))
+
+module E9s
+  module Test
+    module E9s
+
+      class Formtastic < ActiveSupport::TestCase
+        # test "something" do
+        #   assert true
+        # end
+      end
+    
+    end
+  end
+end
