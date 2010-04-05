bf7cad95fea931b2f116100022ec326c541685d8
Paul Engel <paulengel@ArCHanGeL.local>
2010-04-06 00:41:33 +0200

    Added more test assertions for combined translations (e.g. "Male / Female".t equals "Man / Vrouw")

--- test/plugin.rb
+++ test/plugin.rb
@@ -29,11 +29,16 @@ module E9s
       end
       
       test "combined_translations" do
-        assert_equal "Meer huizen" , "More houses".t
-        assert_equal "MEER STRATEN", "MORE STREETS".t
-        assert_equal "meer vragen" , "more questions".t
-        assert_equal "één huis"    , "one house".t
-        # assert_equal "Één huis"    , "One house".t
+        assert_equal "Meer huizen"        , "More houses".t
+        assert_equal "MEER STRATEN"       , "MORE STREETS".t
+        assert_equal "meer vragen"        , "more questions".t
+        assert_equal "meer vragen"        , "more questions".t
+        assert_equal "Vragen & Antwoorden", "Questions & Answers".t
+        assert_equal "vraag & antwoord"   , "question & answer".t
+        assert_equal "Vraag & antwoord"   , "Question & answer".t
+        assert_equal "Man / Vrouw"        , "Male / Female".t
+        assert_equal "één huis"           , "one house".t
+        # assert_equal "Één huis"           , "One house".t
       end
     end
     
