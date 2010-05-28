13abc44f0f45cafea26d697f632d86dd8fcca878
Paul Engel <paul.engel@holder.nl>
2010-05-28 18:25:55 +0200

    Implemented meta data rendering within views

---
+++ lib/e9s/action_view.rb
@@ -0,0 +1,64 @@
+
+module ActionView
+  class Base
+    def render_with_e9s(*args, &block)
+      old_value, String.capturing_template_content = String.capturing_template_content, true
+      ::E9s::ActionView.sanitize_html render_without_e9s(*args, &block)
+    ensure
+      String.capturing_template_content = old_value
+    end
+    alias_method_chain :render, :e9s
+  end
+
+end
+
+module E9s
+  module ActionView
+    extend self
+    
+    require "hpricot"
+    
+    def sanitize_html(html)
+      return html unless (html || "").include?("<i18n ")
+      
+      doc = Hpricot html
+
+      (doc/"head i18n").each do |i18n|
+        i18n.swap i18n.inner_html
+      end
+
+      (doc/"body i18n").each do |i18n|
+        elem = Hpricot::Elem.new "span", i18n.raw_attributes.merge({:class => "i18n"})
+        elem.inner_html = i18n.inner_html
+        i18n.swap elem.to_html
+      end
+
+      (doc/"body input").each do |input|
+        sanitize_input(input)
+      end
+
+      (doc/"body textarea").each do |input|
+        sanitize_input(input)
+      end
+
+      doc.to_html
+    end
+    
+    def sanitize_input(input)
+      %w(value seatholder).each do |input_attr|
+        next unless input.attributes[input_attr].include?("<i18n ")
+
+        i18n = Hpricot(input.attributes[input_attr]).children.first
+        i18n.raw_attributes.each do |key, value|
+          input.attributes[key] = value
+        end
+        
+        input.attributes[input_attr] = i18n.inner_html
+        input.attributes["class"] = ["i18n", input.attributes["class"]].uniq.join(" ").strip
+      end
+        
+      input
+    end
+    
+  end
+end
--- lib/e9s/string.rb
+++ lib/e9s/string.rb
@@ -1,27 +1,36 @@
 
 class String
+  cattr_accessor :capturing_template_content
+  
   def upcase_first
     empty? ?
       self :
       self[0].chr.capitalize + self[1, size]
   end
   
-  def upcase_first!
-    self     == (result = upcase_first)       ? nil : replace(result)
-  end
-  
-  def pluralize!
-    downcase == (result = pluralize).downcase ? nil : replace(result)
-  end
-  
   def cp_case(s)
     send((:downcase unless s.dup.downcase!) || (:upcase unless s.dup.upcase!) || (:upcase_first unless s.dup.upcase_first!) || :to_s)
   end
   
+  def upcase_first!
+    self == (result = upcase_first) ? nil : replace(result)
+  end
+  
+  def cp_case!(s)
+    self == (result = cp_case(s))   ? nil : replace(result)
+  end
+  
+  def singularize!
+    downcase == (result = singularize).downcase ? nil : replace(result)
+  end
+  
+  def pluralize!
+    downcase == (result = pluralize).downcase   ? nil : replace(result)
+  end
+  
   def t(options = {})
     self.split(" ").collect do |string|
-      
-      key              = string.include?(".") ? string : "word.#{string}"
+      key              = string.include?(".") ? string.dup : "word.#{string}"
       default          = key.split(".").last
       translating_word = key.starts_with?("word.")
     
@@ -32,23 +41,32 @@ class String
         options[:default] = [options[:default]].flatten << default.humanize
         s = i18n_t key, options
         s = s[:_base] if s.is_a?(Hash)
+        value = s.dup
       else
         s = i18n_t key, options.merge({:default => translating_word ? "" : default.humanize})
+        value = s.dup
     
         if translating_word
           unless translated = !s.empty?
-            s = i18n_t key.singularize, options.merge({:default => ""})
+            key.singularize!
+            s = i18n_t key, options.merge({:default => ""})
+            value = s.dup
           end
       
           if s.empty?
             s = default.humanize
+            value = s.dup
           else
             s = s.pl(options[:count]) unless !options[:pluralize] or (options[:count].nil? and default.dup.pluralize!)
           end
         end
       end
-    
-      s.gsub!(/^=\s+/, "") ? s : s.cp_case(options[:capitalize] ? default.capitalize : default)
+      
+      unless s.gsub!(/^=\s+/, "")
+        s.cp_case! options[:capitalize] ? default.capitalize : default
+      end
+
+      String.capturing_template_content ? taggify(string, s, key, value) : s
       
     end.join " "
   end
@@ -73,6 +91,11 @@ private
     end
     
     I18n.t key, options
+  end
+  
+  def taggify(key, value, actual_key, actual_value)
+    attrs = {"data-key" => key, "data-actual_key" => actual_key, "data-actual_value" => actual_value}.collect{|k, v| "#{k} = #{v.inspect}"}.join " "
+    "<i18n #{attrs}>#{value}</i18n>"
   end
   
 end
