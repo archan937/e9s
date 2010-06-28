ee719b002f80d978e3c61840fcad69bf52f1ae34
Paul Engel <paulengel@ArCHanGeL.local>
2010-06-29 01:11:04 +0200

    Updated README.textile Added the 'pl' method to Symbol

--- lib/e9s/core/symbol.rb
+++ lib/e9s/core/symbol.rb
@@ -1,6 +1,8 @@
 
+require "e9s/core/symbol/inflections"
 require "e9s/core/symbol/internationalization"
 
 class Symbol
+  include E9s::Core::Symbol::Inflections
   include E9s::Core::Symbol::Internationalization
 end
---
+++ lib/e9s/core/symbol/inflections.rb
@@ -0,0 +1,14 @@
+
+module E9s
+  module Core
+    module Symbol
+      module Inflections
+        
+        def pl(count = nil)
+          to_s.pl count
+        end
+        
+      end
+    end
+  end
+end
