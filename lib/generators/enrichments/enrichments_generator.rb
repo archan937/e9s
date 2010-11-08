446e65b6e44c5e6259a2c17f1101b90d8b13e2a9
Paul Engel <paulengel@archan937.local>
2010-11-09 00:29:24 +0100

    Minor tweak in the Rails generator

--- lib/generators/enrichments/enrichments_generator.rb
+++ lib/generators/enrichments/enrichments_generator.rb
@@ -4,7 +4,7 @@ class EnrichmentsGenerator < Rails::Generators::Base
   class_option :user_model       , :type => :string , :default => "User"       , :aliases => "-u" , :desc => "The name of the Authlogic user model."
   class_option :content_model    , :type => :string , :default => "CmsContent" , :aliases => "-c" , :desc => "The name of the CMS content model."
   class_option :translation_model, :type => :string , :default => "Translation", :aliases => "-t" , :desc => "The name of the I18n translation model."
-  class_option :migrate          , :type => :boolean, :default => false        , :aliases => "-m" , :desc => "Run 'rake db:migrate' after generating model and migration"
+  class_option :migrate          , :type => :boolean, :default => false        , :aliases => "-m" , :desc => "Run 'rake db:migrate' after generating model and migration."
 
   def generate_rich_cms_files
     generate "rich:authlogic_user", options[:user_model]
